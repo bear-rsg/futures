8479e17ca7264de44ce9995ecb847d386021a3d5
Mike Allaway <m.j.allaway@bham.ac.uk>
2023-08-31 10:05:13 +0100

    added welcome content and various tweaks

--- django/general/urls.py
+++ django/general/urls.py
@@ -5,6 +5,5 @@ app_name = 'general'
 
 urlpatterns = [
     path('', views.WelcomeTemplateView.as_view(), name='welcome'),
-    path('about/', views.AboutTemplateView.as_view(), name='about'),
     path('cookies/', views.CookiesTemplateView.as_view(), name='cookies')
 ]
